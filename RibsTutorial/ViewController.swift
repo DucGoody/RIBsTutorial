6cbbcaad07a98cde7dd1d424bf700f39e8c04448
HoangVanDuc <hoangvanduc101095@gmail.com>
2019-12-16 17:09:22 +0700

    Initial Commit

---
+++ RibsTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RibsTutorial
+//
+//  Created by DUCHV3 on 12/16/19.
+//  Copyright © 2019 DUCHV3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
